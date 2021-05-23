f734ac63fb56be5b1f8df105bb251d35f5b3cc25
kalinkaaaa14 <61375322+kalinkaaaa14@users.noreply.github.com>
2021-05-23 10:43:52 +0300

    Init Backend Project

---
+++ confectionary/src/test/java/com/university/confectionary/ConfectionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.confectionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfectionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
