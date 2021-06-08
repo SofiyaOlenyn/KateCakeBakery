cae57335b9f41f0463a060969cec5c01255a8aec
kalinkaaaa14 <kalinkaaaa14@gmail.com>
2021-06-08 17:10:22 +0300

    get catalog endpoint done

--- confectionary/src/main/java/com/university/confectionary/controller/ProductController.java
+++ confectionary/src/main/java/com/university/confectionary/controller/ProductController.java
@@ -1,11 +1,21 @@
 package com.university.confectionary.controller;
 
+import com.university.confectionary.dto.CatalogResponseDto;
+import com.university.confectionary.service.ProductService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
+    @RequestMapping(value = "/catalog/{productType}", method = RequestMethod.GET)
+    public ResponseEntity<CatalogResponseDto> catalog(
+            @PathVariable final String productType
+    ) {
+        return productService.getProductByType(productType);
+    }
 }
