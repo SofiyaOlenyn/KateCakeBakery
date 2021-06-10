6220a620289a84c943cfbd5e7c87b34178f598d2
valeriia-lun <61375793+valeriia-lun@users.noreply.github.com>
2021-06-10 14:54:29 +0300

    created registration and added permissions

---
+++ confectionary/src/main/java/com/university/confectionary/controller/RegistrationController.java
@@ -0,0 +1,43 @@
+package com.university.confectionary.controller;
+
+import com.kma.practice8.springsecuritycustom.dto.RegistrResponseDto;
+import com.kma.practice8.springsecuritycustom.dto.UserCredentialsDto;
+import com.kma.practice8.springsecuritycustom.Utils.ResponseGenerator;
+import com.kma.practice8.springsecuritycustom.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    private final ResponseGenerator responseGenerator;
+    private final UserService userService;
+
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ResponseEntity<RegistrResponseDto> addFavorite(
+            @Valid @RequestBody final UserCredentialsDto userCredentialsDto
+    ) {
+        if (userService.createUser(userCredentialsDto.getUsername(), userCredentialsDto.getPassword())) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .header(HttpHeaders.AUTHORIZATION, "generated-jwt-token")
+                    .body(responseGenerator.makeRegisterResponse("Account created"));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .header(HttpHeaders.AUTHORIZATION, "generated-jwt-token")
+                    .body(responseGenerator.makeRegisterResponse("User already exists"));
+        }
+    }
+
+}
