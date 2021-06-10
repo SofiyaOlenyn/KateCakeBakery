438e8b387920c1f91c7a971e994ee932f8fa81d9
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-06-10 18:46:09 +0300

    added order initial component

--- KateCake-front/src/app/app.module.ts
+++ KateCake-front/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {AuthComponent} from "./auth/auth.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    CartComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
