7013a9fe2135c77e958906974cecf4266141bb47
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-06-11 08:11:42 +0300

    added add product method

--- KateCake-front/src/app/catalog-list/add-product/add-product.component.ts
+++ KateCake-front/src/app/catalog-list/add-product/add-product.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {DataStorageService} from "../../shared/data-storage.service";
+import {CategoriesEnum} from "../../shared/constants/categories.constant";
 
 @Component({
   selector: 'app-add-product',
@@ -35,6 +36,36 @@ export class AddProductComponent implements OnInit {
   }
 
   async add(): Promise<void> {
+    const name = this.addProduct.get('name').value;
+    const mainPhoto = this.addProduct.get('mainPhoto').value;
+    const ingredients = this.addProduct.get('ingredients').value;
+    const supplements = this.addProduct.get('supplements').value;
+    const price = this.addProduct.get('price').value;
+    const weight = this.addProduct.get('weight').value;
+
+    this.route.queryParams.subscribe(async params => {
+      const productType = params.type as CategoriesEnum;
+
+      const response = await this.dataStorage.fetchAddProduct(
+        localStorage.getItem('token'),
+        {
+          name,
+          mainPhoto,
+          productType: CategoriesEnum[productType].toLowerCase(),
+          ingredients,
+          supplements,
+          price,
+          weight
+        });
+
+      (response.id)
+        ? this.router.navigate(['/catalog'], {
+          queryParams: {
+            type: productType
+          }
+        })
+        : alert('Data is incorrect, try again!');
+    });
   }
 
 }
