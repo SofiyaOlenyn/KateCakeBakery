ff00b7ec5b721ac12095980a67e6a3436db6a536
kalinkaaaa14 <kalinkaaaa14@gmail.com>
2021-06-11 21:09:26 +0300

    configuration added

--- confectionary/src/main/java/com/university/confectionary/config/WebSecurityConfig.java
+++ confectionary/src/main/java/com/university/confectionary/config/WebSecurityConfig.java
@@ -1,10 +1,9 @@
 package com.university.confectionary.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.http.HttpMethod.POST;
 import com.university.confectionary.repositories.UserRepository;
+import com.university.confectionary.service.MyUserDetailsService;
 import com.university.confectionary.utils.JwtTokenGenerator;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,9 +19,10 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import static org.springframework.http.HttpMethod.POST;
 
 @RequiredArgsConstructor
 @Configuration
@@ -42,19 +42,46 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/order").authenticated()
                 .antMatchers(HttpMethod.DELETE, "/product/**").authenticated()
                 .antMatchers(HttpMethod.PATCH, "/product").authenticated()
                 .antMatchers(POST, "/product").authenticated()
                 .anyRequest().permitAll()
-            .and()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
+                .and()
                 .addFilter(logoutFilter())
                 .addFilterBefore(customLoginFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new AuthenticationFilter(userDetailsService(), jwtTokenGenerator), CustomLoginFilter.class)
                 .addFilterBefore(corsFilter(), SessionManagementFilter.class); //adds your custom CorsFilter
     }
 
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return new MyUserDetailsService(userRepository);
+    }
+
+    @Bean
+    @SneakyThrows
+    @Override
+    public AuthenticationManager authenticationManagerBean() {
+        return super.authenticationManagerBean();
+    }
+
+    private CustomLoginFilter customLoginFilter() {
+        final CustomLoginFilter customLoginFilter =  new CustomLoginFilter(authenticationManagerBean(), objectMapper, jwtTokenGenerator);
+        customLoginFilter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/my-login", POST.name()));
+
+        return customLoginFilter;
+    }
+
+    private LogoutFilter logoutFilter() {
+        final LogoutHandler[] logoutHandlers = new LogoutHandler[] {
+                new SecurityContextLogoutHandler()
+        };
+
+        return new LogoutFilter(new LogoutSuccessHandlerAdapter(), logoutHandlers);
+    }
 }
