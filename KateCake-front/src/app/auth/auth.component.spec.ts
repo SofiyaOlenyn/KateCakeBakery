82da22a6f562f96a976057b22a03c3d1959b6714
Daryna Kupchyk <dasha.kupchik28@gmail.com>
2021-06-14 14:32:46 +0300

    fixed order form

---
+++ KateCake-front/src/app/auth/auth.component.spec.ts
@@ -0,0 +1,30 @@
+// let component: BannerComponent;
+// let fixture: ComponentFixture<BannerComponent>;
+// let h1: HTMLElement;
+//
+// beforeEach(() => {
+//   TestBed.configureTestingModule({
+//     declarations: [ BannerComponent ],
+//   });
+//   fixture = TestBed.createComponent(BannerComponent);
+//   component = fixture.componentInstance; // BannerComponent test instance
+//   h1 = fixture.nativeElement.querySelector('h1');
+// });
+
+// describe('LightswitchComp', () => {
+//   it('#clicked() should toggle #isOn', () => {
+//     const comp = new LightswitchComponent();
+//     expect(comp.isOn).toBe(false, 'off at first');
+//     comp.clicked();
+//     expect(comp.isOn).toBe(true, 'on after click');
+//     comp.clicked();
+//     expect(comp.isOn).toBe(false, 'off after second click');
+//   });
+//
+//   it('#clicked() should set #message to "is on"', () => {
+//     const comp = new LightswitchComponent();
+//     expect(comp.message).toMatch(/is off/i, 'off at first');
+//     comp.clicked();
+//     expect(comp.message).toMatch(/is on/i, 'on after clicked');
+//   });
+// });
